85b7052926f07d86b0b6935054887a0f3a39a140
sele14 <eirik.selander@gmail.com>
2022-11-25 23:15:34 +0000

    2020 C# AOC

---
+++ day-01/Program.cs
@@ -0,0 +1,51 @@
+﻿class Day1
+{
+    public static void Main()
+    {
+        string[] lines = System.IO.File.ReadAllLines(@".\input.txt");
+
+        // convert input to numbers
+        List<int> linesNum = (Array.ConvertAll(lines, s => Int32.Parse(s))).ToList();
+        linesNum.Sort();
+
+        Console.WriteLine("########## Day 1 2020 ##########");
+        Console.WriteLine($"Part one solution: {PartOne(linesNum)}");
+        Console.WriteLine($"Part two solution: {PartTwo(linesNum)}");
+        Console.WriteLine("################################");
+
+
+        static int PartOne(List<int> list)
+        {
+            foreach (int x in list)
+            {
+                foreach (int y in list)
+                {
+                    if (x + y == 2020)
+                    {
+                        return x * y;
+                    }
+                }
+            }
+            return 0;
+        }
+
+        static int PartTwo(List<int> list)
+        {
+            foreach (int x in list)
+            {
+                foreach (int y in list)
+                {
+                    foreach (int z in list)
+                    {
+                        if (x + y + z == 2020)
+                        {
+                            return x * y * z;
+                        }
+                    }
+                }
+            }
+            return 0;
+        }
+
+    }
+}
---
+++ day-02/Program.cs
@@ -0,0 +1,39 @@
+﻿public class Day2
+{
+    public static void Main()
+    {
+        int valid = 0;
+        string[] lines = System.IO.File.ReadAllLines(@".\input.txt");
+        foreach (string line in lines)
+        {
+            // get rule
+            string rule = line.Split(':')[0];
+            string[] ruleRange = rule.Split(' ')[0].Split('-');
+            char ruleLetter = char.Parse(rule.Split(' ')[1]);
+
+            // convert rule range to numbers
+            List<int> ruleRangeNums = (Array.ConvertAll(ruleRange, s => Int32.Parse(s.Trim()))).ToList();
+            int ruleRangeMin = ruleRangeNums.Min();
+            int ruleRangeMax = ruleRangeNums.Max();
+
+            // get password
+            string password = line.Split(':')[1].Trim();
+
+            // get frequency of required letter in password
+            int letterFreq = password.Count(f => (f == ruleLetter));
+
+            // check if password is valid
+            // 1. check if pw contains the specified letter
+            if (password.Contains(ruleLetter))
+            {
+                // 2. if it contains it, check if the allowed letter within allowed range
+                if (ruleRangeMin <= letterFreq && letterFreq <= ruleRangeMax)
+                {
+                    valid += 1;
+                }
+            }
+        }
+        Console.WriteLine($"Number of valid passwords: {valid}");
+    }
+
+}
