1486e4174b3812d4a9e4633a4c8abf850f1627b6
sele14 <eirik.selander@gmail.com>
2022-12-04 22:56:28 +0000

    part two and refactor

--- 2022/day-01/Program.cs
+++ 2022/day-01/Program.cs
@@ -5,10 +5,11 @@
         string[] lines = System.IO.File.ReadAllLines(@".\input.txt");
         Console.WriteLine("########## Day 1 2022 ##########");
         Console.WriteLine($"Part one solution: {PartOne(lines)}");
-        // Console.WriteLine($"Part two solution: {PartTwo(linesNum)}");
+        Console.WriteLine($"Part two solution: {PartTwo(lines)}");
         Console.WriteLine("################################");
 
-        static int PartOne(string[] lines)
+        // helper function to get the sum of all elves' calories
+        static List<int> SumOfCalories(string[] lines)
         {
             // create sublist for each elf
             var elfCals = lines.Aggregate(new List<List<string>> { new List<string>() },
@@ -36,9 +37,26 @@
                 // add to a list
                 elfCaloriesSum.Add(elfCalSum);
             }
+            return elfCaloriesSum;
+        }
+
+        static int PartOne(string[] lines)
+        {
+            var elfCaloriesSum = SumOfCalories(lines);
             // return the largest sum of calories
             return elfCaloriesSum.Max();
         }
+        static int PartTwo(string[] lines)
+        {
+            var elfCaloriesSum = SumOfCalories(lines);
+            // return the three largest sum of calories
+            elfCaloriesSum.Sort();
+            // move largest to the front
+            elfCaloriesSum.Reverse();
+            // return the sum of the first three elements (largest)
+            return elfCaloriesSum.Take(3).Sum();
+        }
+
 
 
     }
