195291d4037bcf0226366bbbe10aa61bda28ab6e
sele14 <eirik.selander@gmail.com>
2022-12-10 20:30:03 +0000

    day 2 prt 2

--- 2022/day-02/Program.cs
+++ 2022/day-02/Program.cs
@@ -5,7 +5,7 @@
         string[] lines = System.IO.File.ReadAllLines(@".\input.txt");
         Console.WriteLine("--------- Day 2 2022 ----------");
         Console.WriteLine($"Part one solution: {PartOne(lines)}");
-        // Console.WriteLine($"Part two solution: {PartTwo(lines)}");
+        Console.WriteLine($"Part two solution: {PartTwo(lines)}");
         Console.WriteLine("-------------------------------");
 
         // function that can determine number of points a  player wins in rock paper scissors (RPS) per round
@@ -78,6 +78,74 @@
             return 0;
         }
 
+        // same as above except now X == lose, Y==draw, Z==win
+        static int RPSResult(string playerOne, string outcome)
+        {
+            if (playerOne == "A")
+            {
+                // rock + lose
+                if (outcome == "X")
+                {
+                    // scissors (3) + lose (0)
+                    return 3 + 0;
+                }
+                // rock + draw
+                else if (outcome == "Y")
+                {
+                    // rock (1) + draw (3)
+                    return 1 + 3;
+                }
+                // rock + win
+                else if (outcome == "Z")
+                {
+                    // paper (2) vs win (6)
+                    return 2 + 6;
+                }
+            }
+            else if (playerOne == "B")
+            {
+                // paper + loss
+                if (outcome == "X")
+                {
+                    // rock (1) + loss (0)
+                    return 1 + 0;
+                }
+                // paper + draw
+                else if (outcome == "Y")
+                {
+                    // paper (2) + draw (3)
+                    return 2 + 3;
+                }
+                // paper + win
+                else if (outcome == "Z")
+                {
+                    // scissors (3) vs win (6)
+                    return 3 + 6;
+                }
+            }
+            else
+            {
+                // scissors + loss
+                if (outcome == "X")
+                {
+                    // paper (2) + loss (0)
+                    return 2 + 0;
+                }
+                // scissors + draw
+                else if (outcome == "Y")
+                {
+                    // scissors (3) vs draw (3)
+                    return 3 + 3;
+                }
+                // scissors + win
+                else if (outcome == "Z")
+                {
+                    // rock (1) + win (6)
+                    return 1 + 6;
+                }
+            }
+            return 0;
+        }
         static int PartOne(string[] rounds)
         {
             int score = 0;
@@ -88,6 +156,19 @@
                 string playerOne = plays[0];
                 string playerTwo = plays[1];
                 score += RPSSolver(playerOne, playerTwo);
+            }
+            return score;
+        }
+        static int PartTwo(string[] rounds)
+        {
+            int score = 0;
+            foreach (string round in rounds)
+            {
+                // isolate player choice and outcome
+                string[] plays = round.Split(' ');
+                string playerOne = plays[0];
+                string outcome = plays[1];
+                score += RPSResult(playerOne, outcome);
             }
             return score;
         }
